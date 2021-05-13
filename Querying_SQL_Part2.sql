26f79ba9ea00138b5466fda563fd32723c11f8ce
Ammad Rashid <ammad@Ammads-MacBook-Pro-2.local>
2021-05-12 19:26:44 -0700

    updated work

--- Querying_SQL_Part2.sql
+++ Querying_SQL_Part2.sql
@@ -22,6 +22,70 @@ INNER JOIN mass_shootings USING (index);
 
 SELECT * FROM updated_table;
 
--- remove rows which include "-" 
+-- view table
 SELECT * FROM updated_table 
+
+-- Drop location column
+ALTER TABLE updated_table 
+DROP location;
+
+-- view table
+SELECT * FROM updated_table;
+
+-- Replace - with Unknown in order for export to work correctly
+UPDATE updated_table
+SET age_of_shooter = REPLACE(age_of_shooter, '-','Unknown');
+
+-- Replace - with Unknown in the next column 
+UPDATE updated_table
+SET weapons_obtained_legally = REPLACE(weapons_obtained_legally, '-','Unknown');
+
+
+-- view table to see if updates worked correctly 
+SELECT * FROM updated_table;
+
+-- Separate summary column
+SELECT summary 
+INTO case_summary
+FROM updated_table;
+
+SELECT * FROM case_summary;
+
+
+
+-- Drop summary column from updated_table 
+ALTER TABLE updated_table 
+DROP summary;
+
+SELECT * FROM updated_table;
+
+
+UPDATE updated_table
+SET weapons_obtained_legally = 'Yes'
+WHERE index = 28;
+
+SELECT * FROM updated_table;
+
+
+UPDATE updated_table
+SET weapons_obtained_legally = REPLACE(weapons_obtained_legally, ' ','Unknown');
+
+SELECT * FROM updated_table;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
